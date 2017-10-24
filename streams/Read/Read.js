867ac6b8884bfa31c9cc45d68b7de590d2ad0a9f
Andrea Zucchini <azga84@gmail.com>
2017-10-24 11:11:09 +0200

    read and write with streams

---
+++ streams/Read/Read.js
@@ -0,0 +1,6 @@
+const { Readable } = require('stream')
+const inStream = new Readable()
+inStream.push('ABCDEFGHIJKLM')
+inStream.push('NOPQRSTUVWXYZ')
+inStream.push(null) // No more data
+inStream.pipe(process.stdout)
